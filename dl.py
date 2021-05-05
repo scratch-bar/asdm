eb1838e3fa129c1c8a1cdb6e5405eb21d90adb42
waterblock79 <33573572+waterblock79@users.noreply.github.com>
2021-05-05 19:59:46 +0800

    Update dl.py

--- dl.py
+++ dl.py
@@ -2,6 +2,19 @@
 import requests
 import os
 
+print('''
+ _______  _______  ______   __   __ 
+|   _   ||       ||      | |  |_|  |
+|  |_|  ||  _____||  _    ||       |
+|       || |_____ | | |   ||       |
+|       ||_____  || |_|   ||       |
+|   _   | _____| ||       || ||_|| |
+|__| |__||_______||______| |_|   |_|
+     Auto Scratch-Desktop Mirror
+           DOWNLOAD START
+
+''')
+
 # Create 'output'
 if not os.path.exists('output'):
     os.mkdir('output')
@@ -13,6 +26,8 @@ with open(r'./output/scratch-win.exe', "wb") as f:
     for chunk in r.iter_content(chunk_size=512):
         f.write(chunk)
 
+print('Download scratch-desktop for windows done.')
+
 # Download 'Scratch.dmg' for Mac
 
 r = requests.get('https://downloads.scratch.mit.edu/desktop/Scratch.dmg', stream=True)
@@ -20,3 +35,15 @@ r = requests.get('https://downloads.scratch.mit.edu/desktop/Scratch.dmg', stream
 with open(r'./output/scratch-mac.dmg', "wb") as f:
     for chunk in r.iter_content(chunk_size=512):
         f.write(chunk)
+
+print('Download scratch-desktop for macOS done.')
+
+# Get Scratch Version
+
+fl = open("scratch-version","w+")
+fl.write("")
+getVersion = requests.get('https://downloads.scratch.mit.edu/desktop/Scratch%20Setup.exe', allow_redirects=False)
+version = getVersion.headers['location'].split('%20')[1]
+fl.write(version)
+fl.close()
+print('Get version done. version :', version, '. Cost',getVersion.elapsed.total_seconds(),'sec.')
